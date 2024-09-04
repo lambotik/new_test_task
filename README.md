755467b414bdeb7fb49d658acea9ba2eecca262f
lambotik <lambotik1988@gmail.com>
2024-09-04 16:00:20 +0300

    Finish task

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# new_test_task
+# Development of automated tests for the Test task
+
+## Description of the project
+
+| <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg" width="60" heigh="60"/> | <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pytest/pytest-original-wordmark.svg" width="70" heigh="70"/> | <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original-wordmark.svg" width="80" heigh="80"/> | <img src="https://avatars.githubusercontent.com/u/5879127?s=200&v=4" width="60" heigh="60"/> |
+|---------------------------------------------------------------------------------------------------------------------------|---------------------------------------------------------------------------------------------------------------------------|---------------------------------------------------------------------------------------------------------------------|----------------------------------------------------------------------------------------------|
+
+Test automation framework in Python using Pytest + Selenium.
+Generating test reports in Allure report
+## To obtain the rights to launch tests, send nickname github profile in telegram [@lambotik](https://t.me/lambotik)
+___
+* To run test in GitHub Actions click button: [![Run workflows](https://img.shields.io/badge/Run%20workflows-grey)](https://github.com/lambotik/test_task_telegraf/actions/workflows/api.yml) after ckick button [![Run workflows](https://img.shields.io/badge/Run%20workflows-green)](https://github.com/lambotik/test_task_telegraf/actions/workflows/api.yml)
+* To view the current test report, click here: [![Allure Report](https://img.shields.io/badge/Allure%20Report-deployed-orange)](https://lambotik.github.io/test_task_telegraf/)
---
+++ test_example.py
@@ -0,0 +1,70 @@
+import allure
+import pytest
+
+from checking import Checking
+from request import API
+
+
+@allure.epic('Test Api.')
+class TestApi:
+    @allure.suite('GET')
+    @allure.title('test_posts_keys')
+    def test_posts_keys(self):
+        response = API.get_posts()
+        Checking.check_status_code(response, 200)
+        Checking.check_json_all_keys(response, ['userId', 'id', 'title', 'body'])
+
+    @allure.suite('GET')
+    @allure.title('test_posts_by_id')
+    def test_posts_by_id(self):
+        response = API.get_posts_by_id(1)
+        Checking.check_status_code(response, 200)
+        Checking.check_json_value(response, 'id', 1)
+        Checking.check_json_value(response, 'userId', 1)
+
+    @allure.suite('POST')
+    @allure.title('test_post_creating_resource')
+    def test_post_creating_resource(self):
+        body = {'title': 'foo',
+                'body': 'bar',
+                'userId': 1}
+        response = API.post_creating_resource(body=body)
+        Checking.check_status_code(response, 201)
+        Checking.check_json_keys(response, ['title', 'body', 'userId', 'id'])
+        Checking.check_json_value(response, 'title', body['title'])
+        Checking.check_json_value(response, 'body', body['body'])
+        Checking.check_json_value(response, 'userId', body['userId'])
+
+    @allure.suite('PUT')
+    @pytest.mark.parametrize('set_id', (1, 10, 100, 1000, '99', -1))
+    def test_updating_resource(self, set_id):
+        allure.dynamic.title(f'test_updating_resource_by_id_{set_id}')
+        body = {'id': set_id,
+                'title': 'foo',
+                'body': 'bar',
+                'userId': 1}
+        response = API.put_updating_resource(
+            body=body,
+            set_id=set_id)
+        Checking.check_status_code(response, 200)
+        Checking.check_json_value(response, 'id', set_id)
+
+    @allure.suite('DELETE')
+    @pytest.mark.parametrize('set_id', (1, 10, 100, 1000, '99', -1))
+    @pytest.mark.xfail(reason="Some tests should fail due to invalid data.")
+    def test_delete_resource(self, set_id):
+        allure.dynamic.title(f'test_delete_resource_by_id_{set_id}')
+        response = API.delete_resource(set_id=set_id)
+        Checking.check_status_code(response, 200)
+        assert response.text == '{}'
+
+    @allure.suite('DELETE')
+    @pytest.mark.parametrize('set_id', (1, 10, 100, 1000, '99', -1))
+    @pytest.mark.xfail(reason="Some tests should fail due to invalid data.")
+    def test_delete_resource(self, set_id):
+        allure.dynamic.title(f'test_delete_resource_by_id_{set_id}_and_check_it')
+        response = API.delete_resource(set_id=set_id)
+        Checking.check_status_code(response, 200)
+        assert response.text == '{}'
+        response = API.get_posts_by_id(set_id)
+        Checking.check_status_code(response, 404)
